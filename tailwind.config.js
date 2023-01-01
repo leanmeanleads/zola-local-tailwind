53cef70f15248f15052343e1f6159087969a30f4
John Gallagher <john@synapticmishap.co.uk>
2023-01-01 20:28:41 +0000

    fix: look at the Elm JS file for CSS compression

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.{html,js}"],
+  content: ["./templates/**/*.{html,js}", "./static/**/*.js"],
   theme: {
     extend: {},
   },
